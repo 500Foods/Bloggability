3fbb86deb85f6dd2c7f2830d6af1148d5d7feec9
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2024-02-16 14:32:30 -0800

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,16 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/api/', $uri)[1]; 
+$method = $_SERVER['REQUEST_METHOD'];
+
+if(!in_array($method, ['GET','POST'])) {
+  header("HTTP/1.1 405 Method Not Allowed");
+  exit;
+}
+
+require 'bloggable.php'; 
+$blogAPI = new BlogAPI();
+$blogAPI->handleRequest($uri, $method);
+
+?>
