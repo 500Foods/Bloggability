f50860dec6c71e5eb37b0b7257757a9236d705b5
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2024-03-04 00:50:28 -0800

    Update routes.php

--- routes.php
+++ routes.php
@@ -5,20 +5,20 @@ $api_uri = explode('/api/', $uri)[1];
 $method = $_SERVER['REQUEST_METHOD'];
 
 if(!in_array($method, ['GET','POST'])) {
-  header("HTTP/1.1 405 Method Not Allowed");
-  exit;
+    header("HTTP/1.1 405 Method Not Allowed");
+    exit;
 }
 
 require 'bloggability.php';
 $blogAPI = new BlogAPI();
 
 if (strpos($uri, '/rss-') === 0) {
-  $weblog = explode('.', $uri)[0];
-  $weblog = explode('-', $weblog)[1];
-  $blogAPI->handleRequest('RSS', $method, ['weblog' => $weblog]);
+    $weblog = explode('.', $uri)[0];
+    $weblog = explode('-', $weblog)[1];
+    $blogAPI->handleRequest('RSS', $method, ['weblog' => $weblog]);
 
 } else {
-  $blogAPI->handleRequest($api_uri, $method, $_GET, $_POST);
+    $blogAPI->handleRequest($api_uri, $method, array_merge($_GET, $_POST));
 }
 
 ?>
